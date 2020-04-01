4afe533a88065c28e545dde2242261e13bd3698d
tod619 <trevorodwyer619@hotmail.com>
2020-04-01 16:58:52 +0100

    create addTransactionDOM + init function in app.js file

--- app.js
+++ app.js
@@ -16,3 +16,32 @@ const dummyTransactions = [
 ];
 
 let transactions = dummyTransactions;
+
+// Functions
+
+// Add transactions to the DOM
+function addTransactionDOM(transaction) {
+  // Get the amounnt sign
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  const item = document.createElement("li");
+
+  // Add Class based on value
+  item.classList.add(transaction.amount < 0 ? "minus" : "plus");
+
+  item.innerHTML = `
+  ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
+  <button class = "delete-btn">x</button>
+  `;
+
+  list.appendChild(item);
+}
+
+// init app
+function init() {
+  list.innerHTML = "";
+
+  transactions.forEach(addTransactionDOM);
+}
+
+init();
